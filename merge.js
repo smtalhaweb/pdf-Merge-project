74a3c92b8f7e486b19ab207b107db713dff17934
smtalhaweb <s.mtalha1992@gmail.com>
2023-09-24 00:54:22 -0700

    feat: Update PDF files and merge functionality
    - Delete 1.pdf and 2.pdf
    - Add Front-End Developer.pdf and Maintenance Officer.pdf
    - Modify merge.js for improved functionality
    - Delete old merged.pdf
    - Update public/merged.pdf

--- merge.js
+++ merge.js
@@ -3,8 +3,8 @@ const PDFMerger = require('pdf-merger-js');
 var merger = new PDFMerger();
 
 const mergePdfs = async (p1, p2) => {
-  await merger.add('1.pdf');  //merge all pages. parameter is the path to file and filename.
-  await merger.add('2.pdf'); // merge only page 2
+  await merger.add('Front-End Developer.pdf');  //merge all pages. parameter is the path to file and filename.
+  await merger.add('Maintenance Officer.pdf'); // merge only page 2
   
   await merger.save('public/merged.pdf'); //save under given name and reset the internal document
   
